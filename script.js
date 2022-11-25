71a0b42a8e890a1c208bc35d70f847e260982e47
moeidahmed <64657224+moeidahmed@users.noreply.github.com>
2022-11-25 12:24:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,37 @@ function userdata() {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText);
-      let reqdata = data.Info;
-      console.log(reqdata);
+      var reqdata = data.Info;
       document.getElementById("name").innerText = reqdata.name;
       document.getElementById("description").innerText = reqdata.description;
       document.getElementById("views").innerText = reqdata.views;
       document.getElementById("active").innerText = reqdata.active;
       document.getElementById("respond").innerText = reqdata.respond;
-      document.getElementById("about").innerText = reqdata.about;
       document.getElementById("location").innerText = reqdata.location;
+      document.getElementById("about").innerText = reqdata.about;
       document.getElementById("image").innerHTML = `<img 
       src="${reqdata.image}"
-      class="w-60 h-60 border border-white border-4 rounded-full absolute -bottom-10 left-20"
+      class="w-48 h-48  border border-white border-4 rounded-full absolute -bottom-10 left-20"
       />`;
       let skill = reqdata.skills;
       for (let i = 0; i < skill.length; i++) {
-        console.log(skill[i]);
-        document.getElementById("skill").innerHTML += `<button
+        let name = skill[i].name;
+        let more = skill[i].more;
+        console.log(name);
+        console.log(more):
+        document.getElementById(
+          "skill"
+        ).innerHTML += `<button onclick="moreInfo('${name}','${more}')"
         class="bg-gray-600 hover:bg-teal-700 text-white  py-2 px-4 m-1 rounded-full "
-        >${skill[i]}</button>`;
+        >${name}</button>`;
+        //image
       }
     }
   };
   xhttp.open("GET", url, true);
   xhttp.send();
+}
+
+function moreInfo(n, a) {
+  document.getElementById("more").innerHTML = `<b>${n}</b><br> ${a}`;
 }
